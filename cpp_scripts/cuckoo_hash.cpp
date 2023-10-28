3c7982d07e923835f3dc9d4d74030da55868765f
Chia Jen Cheng <chiajenc@ece021.ece.local.cmu.edu>
2023-10-28 18:13:31 -0400

    save progress

--- cpp_scripts/cuckoo_hash.cpp
+++ cpp_scripts/cuckoo_hash.cpp
@@ -10,13 +10,13 @@ struct tuple {
 	int ptr;
 };
 
-struct hash_table {
-	int status = 0; 		 // indicates whether the slot is occupied
-	int tag = 0;             // candidate bucket number of this element
+struct table {
+	int status = 0; 		 	   // indicates whether the slot is occupied
+	int tag = 0;             	   // candidate bucket number of this element
 	struct tuple head[TU_SIZE];    // the entry point to the first tuple of this slot
 };
 
-struct hash_table hash_table[R][C];
+struct table hash_table[R][C];
 
 
 int hash (int function, int key) {
@@ -27,11 +27,26 @@ int hash (int function, int key) {
 }
 
 
-void lookup () {
-	return;
+int lookup (int key) {
+	int index = -1;
+	for (int i = 0; i < C; i++) {
+		if () { index = ; }		
+	}
+	return index;
 }
 
-void insert () {
+void insert (int key) {
+	int hash0 = hash(0, key);
+	int hash1 = hash(1, key);
+	if (hash_table[hash0].status == 0) {
+		hash_table[hash0].status = 1;
+		hash_table[hash0].tag = hash1;
+	} else if (hash_table[hash1].status == 0) {
+		hash_table[hash1].status = 1;
+		hash_table[hash1].tag = hash0;
+	} else {
+		// eviction
+	}
 	return;
 }
 
